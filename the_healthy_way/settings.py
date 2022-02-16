649303a268913d0b6799ec6a7a5762f548c46665
humbertoragazzini <beto_vicio@hotmail.com>
2022-02-16 03:53:15 +0000

    stripe payment working

--- the_healthy_way/settings.py
+++ the_healthy_way/settings.py
@@ -172,6 +172,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+
+#Stripe
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STANDARD_DELIVERY_PERCENTAGE = 10
+
+STRIPE_CURRENCY = 'usd'
+
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY','')
+
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY','')
