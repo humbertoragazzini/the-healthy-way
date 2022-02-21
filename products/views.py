148cc73076118901f9bd92f4d8b81caa852c0e94
humbertoragazzini <beto_vicio@hotmail.com>
2022-02-21 18:44:56 +0000

    added edit, delete, add, functions in admin panel

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Plans, TypeOfPlan, Category
+from .forms import ProductForm, PlansForm
 
 # Create your views here.
 
@@ -57,8 +58,12 @@ def product_detail(request, product_id):
     plan = False
     
     if plan_or_not[0] == product.category:
-        plan = get_object_or_404(Plans, name=product.pk)
-        type_of_plan = get_object_or_404(TypeOfPlan, name=plan.kind_of_plan)
+        try:
+            plan = Plans.objects.get(name=product.pk)
+            type_of_plan = get_object_or_404(TypeOfPlan, name=plan.kind_of_plan)
+        except Plans.DoesNotExist:
+            plan = False
+            type_of_plan = None
 
     else:
         type_of_plan = None
@@ -71,3 +76,104 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    plan = get_object_or_404(Category, name='nutrition_and_workout_plans')
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product_plan = Product.objects.filter(name=form.save())
+            if product_plan[0].category.id == plan.id:
+                product_id = product_plan[0].id
+                print(product_id)
+                return redirect(reverse('add_plan',args=[product_id]))
+            else:
+                messages.success(request, 'Successfully added product!')
+                return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+def add_plan(request, product_id):
+    """ Link plan to product """
+    product = get_object_or_404(Product, pk=product_id)
+    plan = None
+    print(product)
+    if request.method == 'POST':
+        form = PlansForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Link Successfully')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to link the plan. Please ensure the form is valid.')
+    else:
+        try:
+            plan = Plans.objects.get(name=product.pk)
+            form = PlansForm(instance=plan)
+        except Plans.DoesNotExist:
+            form = PlansForm()
+
+    template = 'products/add_plan.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+    plan = get_object_or_404(Category, name='nutrition_and_workout_plans')
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            product_plan = Product.objects.filter(name=form.save())
+            if product_plan[0].category.id == plan.id:
+                product_id = product_plan[0].id
+                return redirect(reverse('add_plan',args=[product_id]))
+            else:
+                messages.success(request, 'Successfully updated product!')
+                return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)    
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+
+    plan = get_object_or_404(Category, name='nutrition_and_workout_plans')
+
+    try:
+        plan = Plans.objects.get(name=product.pk)
+        plan.delete()
+        product.delete()
+    except Plans.DoesNotExist:
+        product.delete()
+
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
